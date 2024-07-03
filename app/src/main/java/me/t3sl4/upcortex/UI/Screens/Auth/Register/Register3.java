2d602fc2f1e46bf0c59196910c449606b79c9fd5
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-07-03 19:40:12 +0300

    Register 3

--- app/src/main/java/me/t3sl4/upcortex/UI/Screens/Auth/Register/Register3.java
+++ app/src/main/java/me/t3sl4/upcortex/UI/Screens/Auth/Register/Register3.java
@@ -1,13 +1,33 @@
 package me.t3sl4.upcortex.UI.Screens.Auth.Register;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.zpj.widget.checkbox.ZCheckBox;
 
 import me.t3sl4.upcortex.R;
 import me.t3sl4.upcortex.UI.Components.NavigationBar.NavigationBarUtil;
 
 public class Register3 extends AppCompatActivity {
+
+    private Button monthlyButton;
+    private Button sixMonthButton;
+    private Button annuallyButton;
+
+    private TextView packagePrice;
+    private Button packagePromotion;
+    private TextView package_spec_1;
+    private TextView package_spec_2;
+    private TextView package_spec_3;
+
+    private ZCheckBox confirmCheckBox;
+    private TextView termsViewText;
+
+    private Button backButton;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +35,32 @@ public class Register3 extends AppCompatActivity {
         setContentView(R.layout.activity_register_3);
 
         NavigationBarUtil.hideNavigationBar(this);
+
+        initializeComponents();
+        buttonClickListeners();
+    }
+
+    private void initializeComponents() {
+        monthlyButton = findViewById(R.id.buttonMonthly);
+        sixMonthButton = findViewById(R.id.button6Month);
+        annuallyButton = findViewById(R.id.buttonAnnually);
+
+        packagePrice = findViewById(R.id.packagePrice);
+        packagePromotion = findViewById(R.id.promotionButton);
+        package_spec_1 = findViewById(R.id.package_spec_1);
+        package_spec_2 = findViewById(R.id.package_spec_2);
+        package_spec_3 = findViewById(R.id.package_spec_3);
+
+        confirmCheckBox = findViewById(R.id.confirmCheckBox);
+        termsViewText = findViewById(R.id.termsViewText);
+
+        backButton = findViewById(R.id.backButton);
+        nextButton = findViewById(R.id.nextButton);
+    }
+
+    private void buttonClickListeners() {
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
