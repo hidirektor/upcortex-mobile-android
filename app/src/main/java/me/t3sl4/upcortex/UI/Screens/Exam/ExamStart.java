b6e75e31b6428ac1e763e72e3cd186f099d0cc71
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-09-18 13:06:24 +0300

    Exam Start & Login Screen Keyboard

--- app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamStart.java
+++ app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamStart.java
@@ -1,15 +1,20 @@
 package me.t3sl4.upcortex.UI.Screens.Exam;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import me.t3sl4.upcortex.Model.Exam.Exam;
 import me.t3sl4.upcortex.R;
+import me.t3sl4.upcortex.Utility.HTTP.Requests.Exam.ExamService;
 
 public class ExamStart extends AppCompatActivity {
 
     private Exam receivedExam;
+
+    private Button startButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +25,36 @@ public class ExamStart extends AppCompatActivity {
 
         initializeComponents();
         buttonClickListeners();
+
+        loadExamDetails();
     }
 
     private void initializeComponents() {
-
+        startButton = findViewById(R.id.startButton);
+        backButton = findViewById(R.id.backButton);
     }
 
     private void buttonClickListeners() {
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+
+        startButton.setOnClickListener(v -> {
+            startExamProcess();
+        });
+    }
+
+    private void loadExamDetails() {
+        if(receivedExam != null) {
+            ExamService.getExamDetail(ExamStart.this, () -> {
+                //Exam datası alındı
+            }, () -> {
+                //Exam datası alınamadı
+            }, receivedExam.getExamID());
+        }
+    }
+
+    private void startExamProcess() {
 
     }
 }
