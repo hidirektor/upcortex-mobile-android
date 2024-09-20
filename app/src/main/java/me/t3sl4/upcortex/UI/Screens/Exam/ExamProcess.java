6cbdd109a1cd96c5ed326261b3424bd11c52d8bb
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-09-21 01:48:08 +0300

    Updated Exam Statistics on Exam Page

--- app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamProcess.java
+++ app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamProcess.java
@@ -6,8 +6,14 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
 
 import me.t3sl4.upcortex.Model.Exam.Exam;
+import me.t3sl4.upcortex.Model.ExamResult.Adapter;
 import me.t3sl4.upcortex.R;
 import me.t3sl4.upcortex.Utility.HTTP.Requests.Exam.ExamService;
 
@@ -20,6 +26,8 @@ public class ExamProcess extends AppCompatActivity {
 
     private LinearLayout examStartLayout;
     private LinearLayout examStatsLayout;
+
+    private RecyclerView examDetailsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,9 @@ public class ExamProcess extends AppCompatActivity {
 
         examStartLayout = findViewById(R.id.examStartLayout);
         examStartLayout.setVisibility(View.GONE);
+
+        examDetailsCard = findViewById(R.id.examDetailsCard);
+        loadTempDetails();
     }
 
     private void buttonClickListeners() {
@@ -60,6 +71,20 @@ public class ExamProcess extends AppCompatActivity {
                 //Exam datası alınamadı
             }, receivedExam.getExamID());
         }
+    }
+
+    private void loadTempDetails() {
+        // Örnek String ve Integer verileri
+        List<String> categoryList = Arrays.asList("Kısa Süreli Bellek", "Uzun Süreli Bellek", "Görsel Bellek", "İşlemsel Bellek");
+        List<String> subNameList = Arrays.asList("Çok İyi Seviye", "İyi Seviye", "Yüksek Tahribat", "Düşük Seviye");
+        List<String> subDescList = Arrays.asList("Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.");
+        List<Integer> percentList = Arrays.asList(75, 60, 90, 45);
+
+        // Adapter ve layout manager ayarlanması
+        Adapter adapter = new Adapter(categoryList, subNameList, subDescList, percentList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        examDetailsCard.setLayoutManager(layoutManager);
+        examDetailsCard.setAdapter(adapter);
     }
 
     private void startExamProcess() {
