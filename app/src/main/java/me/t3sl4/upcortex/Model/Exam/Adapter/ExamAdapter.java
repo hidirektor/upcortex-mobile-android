fae0174634c5fd4c10584f384ef273480dd67006
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-09-21 20:44:09 +0300

    Rename

--- app/src/main/java/me/t3sl4/upcortex/Model/Exam/Adapter/ExamAdapter.java
+++ app/src/main/java/me/t3sl4/upcortex/Model/Exam/Adapter/ExamAdapter.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 import me.t3sl4.upcortex.Model.Exam.Exam;
 import me.t3sl4.upcortex.R;
-import me.t3sl4.upcortex.UI.Screens.Exam.ExamProcess;
+import me.t3sl4.upcortex.UI.Screens.Exam.ExamDashboard;
 
 public class ExamAdapter extends RecyclerView.Adapter<ExamAdapter.ExamViewHolder> {
 
@@ -68,7 +68,7 @@ public class ExamAdapter extends RecyclerView.Adapter<ExamAdapter.ExamViewHolder
         holder.examDescription.setText(examItem.getExamDescription());
 
         holder.itemView.setOnClickListener(v -> {
-            Intent intent = new Intent(context, ExamProcess.class);
+            Intent intent = new Intent(context, ExamDashboard.class);
             intent.putExtra("exam", examItem);
             context.startActivity(intent);
         });
--- app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamProcess.java
+++ app/src/main/java/me/t3sl4/upcortex/UI/Screens/Exam/ExamProcess.java
@@ -1,45 +1,32 @@
 package me.t3sl4.upcortex.UI.Screens.Exam;
 
-import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.core.content.ContextCompat;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.Arrays;
-import java.util.List;
-
-import me.t3sl4.upcortex.Model.Exam.Adapter.ExamResultAdapter;
+import me.t3sl4.upcortex.Model.Exam.Difficulty;
 import me.t3sl4.upcortex.Model.Exam.Exam;
 import me.t3sl4.upcortex.R;
-import me.t3sl4.upcortex.UI.Components.Sneaker.Sneaker;
-import me.t3sl4.upcortex.Utility.HTTP.Requests.Exam.ExamService;
 import me.t3sl4.upcortex.Utility.Screen.ScreenUtil;
 
 public class ExamProcess extends AppCompatActivity {
-
     private Exam receivedExam;
 
-    private Button startButton;
-    private Button backButton;
-    private Button closeExamButton;
+    //DifficultyStars
+    private ImageView difficultyStarOne;
+    private ImageView difficultyStarTwo;
+    private ImageView difficultyStarThree;
 
-    private LinearLayout examStartLayout;
-    private LinearLayout examStatsLayout;
-
-    private RecyclerView examDetailsCard;
+    private int questionTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exam_start);
+        setContentView(R.layout.activity_exam_process);
 
         receivedExam = getIntent().getParcelableExtra("exam");
 
@@ -47,85 +34,33 @@ public class ExamProcess extends AppCompatActivity {
         ScreenUtil.fullScreenMode(ExamProcess.this);
 
         initializeComponents();
-
-        buttonClickListeners();
-
-        loadExamDetails();
     }
 
     private void initializeComponents() {
-        startButton = findViewById(R.id.startButton);
-        backButton = findViewById(R.id.backButton);
-        closeExamButton = findViewById(R.id.closeExamButton);
+        difficultyStarOne = findViewById(R.id.difficultyStarOne);
+        difficultyStarTwo = findViewById(R.id.difficultyStarTwo);
+        difficultyStarThree = findViewById(R.id.difficultyStarThree);
+    }
 
-        examStartLayout = findViewById(R.id.examStartLayout);
-        examStatsLayout = findViewById(R.id.examStatsLayout);
-
-        examDetailsCard = findViewById(R.id.examDetailsCard);
-
-        if(receivedExam.getUserPoint() != 0) {
-            loadTempDetails();
-            examStartLayout.setVisibility(View.GONE);
-            startButton.setVisibility(View.GONE);
-            backButton.setVisibility(View.GONE);
-            examStatsLayout.setVisibility(View.VISIBLE);
-            closeExamButton.setVisibility(View.VISIBLE);
-        } else {
-            examStatsLayout.setVisibility(View.GONE);
-            closeExamButton.setVisibility(View.GONE);
-            examStartLayout.setVisibility(View.VISIBLE);
-            startButton.setVisibility(View.VISIBLE);
-            backButton.setVisibility(View.VISIBLE);
+    private void difficultyMode(Difficulty difficultyEnum) {
+        if(difficultyEnum.name().equals("beginner")) {
+            setStarColor(difficultyStarOne, R.color.warningColor);
+            setStarColor(difficultyStarTwo, R.color.darkOnTop);
+            setStarColor(difficultyStarThree, R.color.darkOnTop);
+        } else if(difficultyEnum.name().equals("medium")) {
+            setStarColor(difficultyStarOne, R.color.warningColor);
+            setStarColor(difficultyStarTwo, R.color.warningColor);
+            setStarColor(difficultyStarThree, R.color.darkOnTop);
+        } else if(difficultyEnum.name().equals("hard")) {
+            setStarColor(difficultyStarOne, R.color.warningColor);
+            setStarColor(difficultyStarTwo, R.color.warningColor);
+            setStarColor(difficultyStarThree, R.color.warningColor);
         }
     }
 
-    private void buttonClickListeners() {
-        backButton.setOnClickListener(v -> {
-            finish();
-        });
+    private void setStarColor(ImageView star, int colorResId) {
+        int color = ContextCompat.getColor(this, colorResId);
 
-        startButton.setOnClickListener(v -> {
-            startExamProcess();
-        });
-
-        closeExamButton.setOnClickListener(v -> {
-            finish();
-        });
-    }
-
-    private void loadExamDetails() {
-        if(receivedExam != null) {
-            ExamService.getExamDetail(ExamProcess.this, () -> {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                String examJson = gson.toJson(receivedExam);
-
-                //Log.d("Exam Details", examJson);
-            }, () -> {
-                Sneaker.with(ExamProcess.this)
-                        .setTitle(getString(R.string.error_title))
-                        .setMessage(getString(R.string.exam_detail_cant_retrieve))
-                        .sneakError();
-            }, receivedExam.getExamID(), receivedExam);
-        }
-    }
-
-    private void loadTempDetails() {
-        // Örnek String ve Integer verileri
-        List<String> categoryList = Arrays.asList("Kısa Süreli Bellek", "Uzun Süreli Bellek", "Görsel Bellek", "İşlemsel Bellek");
-        List<String> subNameList = Arrays.asList("Çok İyi Seviye", "İyi Seviye", "Yüksek Tahribat", "Düşük Seviye");
-        List<String> subDescList = Arrays.asList("Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.", "Görsel bellek yetenekleri mükemmeldir. Görsel bilgileri hızlı bir şekilde öğrenir ve hatırlarlar. Bu, mekan tanıma ve yön bulma gibi günlük görevlerde büyük bir avantaj sağlar.");
-        List<Integer> percentList = Arrays.asList(75, 60, 90, 45);
-
-        // Adapter ve layout manager ayarlanması
-        ExamResultAdapter examResultAdapter = new ExamResultAdapter(categoryList, subNameList, subDescList, percentList);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        examDetailsCard.setLayoutManager(layoutManager);
-        examDetailsCard.setAdapter(examResultAdapter);
-    }
-
-    private void startExamProcess() {
-        Intent examIntent = new Intent(ExamProcess.this, QuestionProcess.class);
-        examIntent.putExtra("exam", receivedExam);
-        startActivity(examIntent);
+        star.setColorFilter(new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN));
     }
 }
